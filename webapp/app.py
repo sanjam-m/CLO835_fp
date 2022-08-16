303797fdb2313b051a9f39cae42b56ce0638f315
meadow-maker <103506273+meadow-maker@users.noreply.github.com>
2022-08-15 21:51:15 -0400

    app.py fixed

--- webapp/app.py
+++ webapp/app.py
@@ -1,11 +1,8 @@
 from flask import Flask
-import os
-import json
-import subprocess
 from flask import render_template
 import socket
 import mysql.connector
-import boto3
+import os
 
 app = Flask(__name__)
 
@@ -13,15 +10,6 @@ DB_Host = os.environ.get('DB_Host') or "localhost"
 DB_Database = os.environ.get('DB_Database') or "mysql"
 DB_User = os.environ.get('DB_User') or "root"
 DB_Password = os.environ.get('DB_Password') or "paswrd"
-clo835 = os.environ.get('clo835')
-group_name = os.environ.get('GROUP_NAME')
-if os.path.exists('/clo835/config/image_url'):
-    s3_url_file = open('/clo835/config/image_url') 
-    json_data = json.load(s3_url_file)
-else: 
-    json_data = {}
-
-print("Background image urls from S3 bucket: ", json_data)
 
 @app.route("/")
 def main():
@@ -31,13 +19,11 @@ def main():
         mysql.connector.connect(host=DB_Host, database=DB_Database, user=DB_User, password=DB_Password)
         color = '#39b54b'
         db_connect_result = True
-        image_url = json_data[""] if json_data else "Not Available"
     except Exception as e:
         color = '#ff3f3f'
         err_message = str(e)
-        image_url = json_data[""] if json_data else "Not Available"
 
-    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color, group=group, image_url=image_url, clo835=clo835)
+    return render_template('hello.html', debug="Environment Variables: DB_Host=" + (os.environ.get('DB_Host') or "Not Set") + "; DB_Database=" + (os.environ.get('DB_Database')  or "Not Set") + "; DB_User=" + (os.environ.get('DB_User')  or "Not Set") + "; DB_Password=" + (os.environ.get('DB_Password')  or "Not Set") + "; " + err_message, db_connect_result=db_connect_result, name=socket.gethostname(), color=color)
 
 @app.route("/debug")
 def debug():
